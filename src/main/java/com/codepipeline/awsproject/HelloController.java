82cb74a8d54b5a5dbba5dc2f52a466ff5783e57b
Pavan Vamsi <vamsi.rhodes@gmail.com>
2019-09-19 17:00:02 -0500

    updated controller,pom and buildspec files

--- src/main/java/com/codepipeline/awsproject/HelloController.java
+++ src/main/java/com/codepipeline/awsproject/HelloController.java
@@ -1,4 +1,15 @@
 package com.codepipeline.awsproject;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
 public class HelloController {
+
+    @GetMapping
+    public String test(){
+        return "Hello Everyone";
+    }
 }
