55e7bb75a44449432e278fed807e26231ab083bd
Pavan Vamsi <vamsi.rhodes@gmail.com>
2019-09-20 13:45:08 -0500

    updated HelloController class

--- src/main/java/com/codepipeline/awsproject/HelloController.java
+++ src/main/java/com/codepipeline/awsproject/HelloController.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello")
+@RequestMapping
 @RestController
 public class HelloController {
 
-    @GetMapping
-    public String test(){
+    @GetMapping("/hello")
+    public String hello(){
         return "Hello.. This is my first pipeline";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello.. This is my first beanstalk instance";
     }
 }
