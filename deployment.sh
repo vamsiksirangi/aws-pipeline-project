c26cf2f4f0ecf7d47776406f5f3e478d6bdf1e8d
vamsiksirangi <vamsi.rhodes@gmail.com>
2020-04-24 15:46:03 -0500

    updated s3 copy command

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum install -y java
 mkdir tmp
 cd tmp
-aws s3 cp s3://mycd-bucket/mytest/target/ . --include "*.war"
-java -jar *.war
+aws s3 cp s3://mycd-bucket/mytest/target/awsproject-1.0-SNAPSHOT.war awsproject-1.0-SNAPSHOT.war
+yum install -y java
+java -jar awsproject-1.0-SNAPSHOT.war
